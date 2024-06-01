b6429c99259844a011c595e1d40d4ae6e1218ee0
sndmndss <yanfedorov120505@gmail.com>
2024-06-02 00:21:04 +0200

    New keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,10 +3,10 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.cmd [[
-    hi Normal guibg=NONE ctermbg=NONE
-]]
 vim.opt.swapfile = false
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open a new tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>") -- previous tab
 
